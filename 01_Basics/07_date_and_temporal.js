ac68102f49ff1c3ea3f338610244c6e53e3457b1
Sanjay Bhandari <107557253+sanjaydbhandari@users.noreply.github.com>
2023-10-14 16:30:02 +0000

    add date file

---
+++ 01_Basics/07_date_and_temporal.js
@@ -0,0 +1,43 @@
+log=console.log
+
+// let date=new Date()
+// console.log(date);
+
+// date format
+// log(date.toString())
+// log(date.toDateString())
+// log(date.toLocaleDateString())
+// log(date.toLocaleTimeString())
+// log(date.toUTCString())
+// log(date.toISOString())
+// log(date.toJSON())
+
+// let dob=new Date(2023,0,12)
+// log("Date of Birth: ",dob.toDateString())
+// dob=new Date(2023,0,12,1,1,1)
+// log("Date of Birth: ",dob.toLocaleString())
+// dob=new Date("2001-01-12")//YYYY-MM-DD
+// log("Date of Birth: ",dob.toLocaleString())
+// dob=new Date("01-12-2001")//MM-DD-YYYY
+// log("Date of Birth: ",dob.toLocaleString())
+
+// --------------------------timestamp------------------------
+
+// dob=new Date("01-12-2001")//MM-DD-YYYY
+// log("Date of Birth: ",dob.toLocaleString())
+// log(dob.getTime())
+
+// let timestamp= Date.now()
+// log(timestamp)
+
+// log(Math.floor(Date.now()/1000))//milisec to sec
+
+// let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay())
+
+// log(newDate.toLocaleString('en-IN',{
+//     month:"long",
+//     hour:"2-digit"
+// }))
